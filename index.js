69ceaa321ec1766be670c0d8ddbb2206b06f8d74
katyarun@gmail.com <katyarun@gmail.com>
2018-05-16 02:27:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function theBeatlesPlay(musicians, instruments) {
   let arr = [];
+  
 }
+
+const arr1 = ["00", "01", "02"];
+const arr2 = ["10", "11", "12"];
+
+for (let i = 0; i < arr1.length; i++) {
+  console.log(arr1[i]);
+  console.log(arr2[i]);
+}
+
