8015f251d8fd9a15c1669c30910cba2a0702d910
katyarun@gmail.com <katyarun@gmail.com>
2018-05-16 02:30:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ function theBeatlesPlay(musicians, instruments) {
   let arr = [];
   for (let i = 0; i < musicians.length; i++) {
     arr.push(`${musicians[i]} plays ${instruments[i]}`);
-    // console.log(arr1[i]);
-}
-}
-
-const arr1 = ["00", "01", "02"];
-const arr2 = ["10", "11", "12"];
-
-for (let i = 0; i < arr1.length; i++) {
-  console.log(arr1[i]);
-  console.log(arr2[i]);
+    console.log(`${musicians[i]} plays ${instruments[i]}`);
+  }
 }
 
+const arr1 = ["John Lennon", "01", "02"];
+const arr2 = ["guitar", "11", "12"];
+
+// for (let i = 0; i < arr1.length; i++) {
+  // console.log(arr1[i]);
+  // console.log(arr2[i]);
+// }
+
+theBeatlesPlay(arr1, arr2);
